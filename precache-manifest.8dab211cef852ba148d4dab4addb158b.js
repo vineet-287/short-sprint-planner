39f4d38d8b8e6e955cfe1c169ffcdcc2cc2d04f2
Vineet Verma <vkv000@gmail.com>
2019-04-07 19:48:05 +0530

    Updates

---
+++ precache-manifest.8dab211cef852ba148d4dab4addb158b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5d2a9a186468c9c4d2c",
+    "url": "/short-sprint-planner/static/css/main.18d549b5.chunk.css"
+  },
+  {
+    "revision": "d5d2a9a186468c9c4d2c",
+    "url": "/short-sprint-planner/static/js/main.6d7124ac.chunk.js"
+  },
+  {
+    "revision": "713114a6291da28065db",
+    "url": "/short-sprint-planner/static/js/runtime~main.d323f115.js"
+  },
+  {
+    "revision": "ff9108822aacf46bfe5a",
+    "url": "/short-sprint-planner/static/css/2.8f45b815.chunk.css"
+  },
+  {
+    "revision": "ff9108822aacf46bfe5a",
+    "url": "/short-sprint-planner/static/js/2.9384099b.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/short-sprint-planner/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/short-sprint-planner/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/short-sprint-planner/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/short-sprint-planner/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/short-sprint-planner/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f75c21e417687eb1baddbfc73934f439",
+    "url": "/short-sprint-planner/index.html"
+  }
+];
